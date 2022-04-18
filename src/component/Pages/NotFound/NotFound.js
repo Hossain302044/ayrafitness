b446f5c43df7d2e3ce6beaa73f795964a13cd376
Hossain302044 <Hossain30204>
2022-04-18 20:48:18 +0600

    notfound page edit

--- src/component/Pages/NotFound/NotFound.js
+++ src/component/Pages/NotFound/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import './Notfound.css'
 
 const NotFound = () => {
     return (
         <div>
-            <h1>404</h1>
+            <Container>
+                <Row>
+                    <div className='notFount'>
+                        <h2>404!</h2>
+                    </div>
+                </Row>
+            </Container>
         </div>
     );
 };
